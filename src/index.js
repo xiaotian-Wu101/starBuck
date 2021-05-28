4e22d4146eca7b9e96d65b1f23818476b8dd01c1
Wuxiaotian <Wuxiaotian101@aliyun.com>
2021-05-28 17:30:02 +0800

    上传了星巴克代码

---
+++ src/index.js
@@ -0,0 +1,32 @@
+let showBtn = document.getElementById("showBtn");
+let list = document.getElementById("list");
+let leftContainer = document.getElementById("left-container");
+let nav = document.getElementById("nav");
+console.log(showBtn);
+console.log(list);
+
+let lock=true;
+showBtn.onclick = function () {
+    
+    if (lock === true) {
+        console.log("列表");
+        list.className = "d-block ";
+        leftContainer.className = "d-none";
+        nav.className = "d-none";
+        showBtn.innerText = "×";
+        showBtn.className = "font-large";
+        lock = false;
+        return ;
+    }
+
+    if (lock === false) {
+        console.log("列表");
+        list.className = "d-none";
+        leftContainer.className = "d-flex align-items-center login ";
+        nav.className = "nav";
+        showBtn.innerText="";
+        showBtn.className = "iconfont icon-ziyuan";
+        lock = true;
+        return ;
+    }
+}
