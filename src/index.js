d015205f54c2671eb10ef383e4058dc36154dc6b
Wuxiaotian <Wuxiaotian101@aliyun.com>
2021-05-29 10:24:32 +0800

    修改了超小屏幕下列表按钮

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ let showBtn = document.getElementById("showBtn");
 let list = document.getElementById("list");
 let leftContainer = document.getElementById("left-container");
 let nav = document.getElementById("nav");
+let xsBtn=document.getElementById("xs");
+console.log(xsBtn);
 console.log(showBtn);
 console.log(list);
 
 let lock=true;
-showBtn.onclick = function () {
+showBtn.onclick =function () {
     
     if (lock === true) {
         console.log("列表");
@@ -27,6 +29,28 @@ showBtn.onclick = function () {
         showBtn.innerText="";
         showBtn.className = "iconfont icon-ziyuan";
         lock = true;
+        return ;
+    }
+}
+let xslock=true;
+xsBtn.onclick=function(){
+    if (xslock === true) {
+        console.log("列表");
+        list.className = "d-block ";
+        nav.className = "d-none";
+        xsBtn.innerText = "×";
+        xsBtn.className = "font-large";
+        xslock = false;
+        return ;
+    }
+
+    if (xslock === false) {
+        console.log("列表");
+        list.className = "d-none";
+        nav.className = "nav";
+        xsBtn.innerText="";
+        xsBtn.className = "iconfont icon-ziyuan";
+        xslock = true;
         return ;
     }
 }
